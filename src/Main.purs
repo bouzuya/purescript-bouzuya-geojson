cb32f65c6fbf6c9ba368471375e259393fd10809
bouzuya <m@bouzuya.net>
2018-12-20 18:45:52 +0900

    add Polygon

--- src/Main.purs
+++ src/Main.purs
@@ -2,11 +2,12 @@ module Main
   ( GeoJSON(..)
   , LineStringJSON
   , PointJSON
+  , PolygonJSON
   , parse
   ) where
 
 import Data.Maybe (Maybe)
-import Foreign (F)
+import Foreign (F, ForeignError(..), fail)
 import Prelude (class Eq, class Show, bind, map, show, (<>), (==))
 import Simple.JSON (class ReadForeign, readImpl)
 import Simple.JSON as SimpleJSON
@@ -19,23 +20,31 @@ type LineStringJSON =
   { type :: String
   , coordinates :: Array (Array Number)
   }
+type PolygonJSON =
+  { type :: String
+  , coordinates :: Array (Array (Array Number))
+  }
 
 data GeoJSON
   = Point PointJSON
   | LineString LineStringJSON
+  | Polygon PolygonJSON
 
 derive instance eqGeoJSON :: Eq GeoJSON
 
 instance readForeignGeoJSON :: ReadForeign GeoJSON where
   readImpl f = do
     o <- readImpl f :: F { type :: String }
-    if o.type == "Point"
-      then map Point (readImpl f :: F PointJSON)
-      else map LineString (readImpl f :: F LineStringJSON)
+    case o.type of
+      "Point" -> map Point (readImpl f :: F PointJSON)
+      "LineString" -> map LineString (readImpl f :: F LineStringJSON)
+      "Polygon" -> map Polygon (readImpl f :: F PolygonJSON)
+      _ -> fail (ForeignError "unknown GeoJSON type")
 
 instance showGeoJSON :: Show GeoJSON where
   show (Point r) = "(Point " <> show r <> ")"
   show (LineString r) = "(LineString " <> show r <> ")"
+  show (Polygon r) = "(Polygon " <> show r <> ")"
 
 parse :: String -> Maybe GeoJSON
 parse = SimpleJSON.readJSON_
